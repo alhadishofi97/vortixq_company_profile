1fa9300aeb37575ddad765a903ac562fbb229ac5
helmi intelix <helmi@intelix.co.id>
2025-09-20 12:49:11 +0700

    contact

--- src/app/modules/contact/controllers/ContactControllers.tsx
+++ src/app/modules/contact/controllers/ContactControllers.tsx
@@ -1,11 +1,42 @@
 import client from "@/app/util/strepiClient";
 
-export const getHome = async () => {
+
+interface Narasi {
+  id: number;
+  title: string;
+  body: string;
+  icon: string;
+}
+
+interface ContactType {
+  id: number;
+  icon: string;
+  title: string;
+  body: string;
+}
+
+export interface ContactData {
+  id: number;
+  narasi: Narasi;
+  contact_type: ContactType[];
+}
+
+
+export const getContact= async () => {
     try {
-        console.log('process.env.REACT_APP_STREPI_CLIENT', process.env.REACT_APP_STREPI_CLIENT);
-        const response = await client.fetch('/api/homes?populate=*');
-        const data = await response.json();
-        return data;
+       const contact = client.collection('ourcontacts');
+        const response = await contact.find({
+            populate: {
+                contactData:{
+                    populate:{
+                        narasi:"*",
+                        contact_type:"*"
+                    }
+                }
+            }
+        });
+    
+        return response.data[0].contactData as ContactData ; // ✅ langsung ar
     } catch (error) {
         console.error('Error fetching home data:', error);
         return null;
--- src/app/modules/contact/views/ContactView.tsx
+++ src/app/modules/contact/views/ContactView.tsx
@@ -1,9 +1,44 @@
 "use client";
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { motion } from 'framer-motion';
 import Threads from '../../../components/animations/Threads';
+import { getContact } from '../controllers/ContactControllers';
+import SvgRenderer from '@/app/util/svgRendered';
+
+
+
+interface Narasi {
+  id: number;
+  title: string;
+  body: string;
+  icon: string;
+}
+
+interface ContactType {
+  id: number;
+  icon: string;
+  title: string;
+  body: string;
+}
+
+export interface ContactData {
+  id: number;
+  narasi: Narasi;
+  contact_type: ContactType[];
+}
 
 const ContactView: React.FC = () => {
+const [contact, setContact] = useState<ContactData | null>(null);
+
+useEffect(() => {
+  async function fetchData() {
+    const data = await getContact();
+    setContact(data); // ✅ langsung object
+    console.log('datacontactcontactcontactcontact',data)
+  }
+  fetchData();
+}, []);
+
   const [formData, setFormData] = useState({
     name: '',
     email: '',
@@ -78,17 +113,16 @@ const ContactView: React.FC = () => {
           className="lg:col-span-2 bg-black/60 backdrop-blur-md rounded-2xl p-6 border border-white/10"
         >
           <h2 className="text-lg xs:text-xl sm:text-2xl md:text-3xl lg:text-4xl font-semibold text-white mb-4">
-            Contact Information
+            {contact?.narasi.title}
           </h2>
           
           <p className="text-slate-300 text-sm xs:text-base sm:text-lg leading-relaxed mb-6">
-            Ready to transform your business with cutting-edge AI and robust cybersecurity solutions? 
-            Reach out to our team of experts today.
+            {contact?.narasi.body}
           </p>
 
           <div className="space-y-4">
             {/* Email */}
-            <div className="flex items-start gap-4">
+            {/* <div className="flex items-start gap-4">
               <div className="flex-shrink-0 w-6 h-6 bg-blue-500/20 rounded-lg flex items-center justify-center">
                 <svg className="w-4 h-4 text-blue-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 8l7.89 4.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z" />
@@ -98,10 +132,10 @@ const ContactView: React.FC = () => {
                 <h3 className="text-white font-medium text-lg">Email</h3>
                 <p className="text-slate-300">contact@vortiqx.com</p>
               </div>
-            </div>
+            </div> */}
 
             {/* Phone */}
-            <div className="flex items-start gap-4">
+            {/* <div className="flex items-start gap-4">
               <div className="flex-shrink-0 w-6 h-6 bg-green-500/20 rounded-lg flex items-center justify-center">
                 <svg className="w-4 h-4 text-green-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M3 5a2 2 0 012-2h3.28a1 1 0 01.948.684l1.498 4.493a1 1 0 01-.502 1.21l-2.257 1.13a11.042 11.042 0 005.516 5.516l1.13-2.257a1 1 0 011.21-.502l4.493 1.498a1 1 0 01.684.949V19a2 2 0 01-2 2h-1C9.716 21 3 14.284 3 6V5z" />
@@ -111,10 +145,10 @@ const ContactView: React.FC = () => {
                 <h3 className="text-white font-medium text-lg ">Phone</h3>
                 <p className="text-slate-300 ">+60-123 563 137</p>
               </div>
-            </div>
+            </div> */}
 
             {/* Office */}
-            <div className="flex items-start gap-4">
+            {/* <div className="flex items-start gap-4">
               <div className="flex-shrink-0 w-6 h-6 bg-purple-500/20 rounded-lg flex items-center justify-center">
                 <svg className="w-4 h-4 text-purple-400" fill="none" stroke="currentColor" viewBox="0 0 24 24">image.png
                   <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
@@ -127,7 +161,25 @@ const ContactView: React.FC = () => {
                   C4-3-5 Solaris Dutamas No. 1 Jalan Dutamas 50480, Kuala Lumpur
                 </p>
               </div>
-            </div>
+            </div> */}
+
+            {
+              contact?.contact_type.map((val, i) => {
+                return (
+                    <div key={i} className="flex items-start gap-4">
+                      <div className="flex-shrink-0 w-6 h-6 bg-purple-500/20 rounded-lg flex items-center justify-center">
+                      <SvgRenderer svgString={val.icon}/>
+                      </div>
+                      <div>
+                        <h3 className="text-white font-medium text-lg ">{val.title}</h3>
+                        <p className="text-slate-300 ">
+                          {val.body}
+                        </p>
+                      </div>
+                    </div>
+                )
+              })
+            }
           </div>
         </motion.div>
 
